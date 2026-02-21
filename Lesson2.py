fb98f706db5ca1eac87fd16198ecc9a7d7c677bc
Admin <limakrukynova@gmail.com>
2026-02-21 17:36:57 +0200

    lesson2.py commit2

--- Lesson2.py
+++ Lesson2.py
@@ -9,7 +9,7 @@ def назва_функції(аргументи):
 from hashlib import new
 
 # однорядковий коментар
-"""
+
 from hashlib import new
 
 
@@ -28,3 +28,21 @@ def second_function():
 print(second_function)
 print(second_function())
 #print(hello) ---> print("Hello Students!", "Serish!")
+
+def hello(arg_1, arg_2):
+    return  arg_1 + arg_2
+print(hello)
+print(hello("Hello", "World1"))
+print(hello(3,5))
+print(hello(input("arg_1-"), input("arg_2-")))
+
+x = "IT"
+y = " Step"
+print(hello(x,y))
+"""
+def s_triangle(a, h):
+    s = .5 * a * h
+    return s
+print(f"площа трикутника s = {s_triangle(5,6)}")
+print(f"площа трикутника s ="
+      f"{s_triangle(int(input('a=')),int(input('h=')))}")
