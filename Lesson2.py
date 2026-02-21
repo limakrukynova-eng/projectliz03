6dbb46cd71235a290d449aef0f3cd9d5e019a094
Admin <limakrukynova@gmail.com>
2026-02-21 17:13:20 +0200

    lesson2.py commit

---
+++ Lesson2.py
@@ -0,0 +1,30 @@
+
+"""
+def назва_функції(аргументи):
+    код функції
+
+
+назва_функції(значення)
+
+from hashlib import new
+
+# однорядковий коментар
+"""
+from hashlib import new
+
+
+def first_function():
+    print('Hello Students!')
+
+
+first_function()
+print('first function')
+
+def second_function():
+    hello = "Hello Students!"
+    name = input("What is your name?")
+    return hello, name
+
+print(second_function)
+print(second_function())
+#print(hello) ---> print("Hello Students!", "Serish!")
